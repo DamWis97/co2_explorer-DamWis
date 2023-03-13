a21ef632a6cdd7bc91b8bc8d212a6eaab80a6a13
DamWis97 <104360045+DamWis97@users.noreply.github.com>
2023-03-13 11:32:41 +0100

    Update co2_explorer.py

--- co2_explorer.py
+++ co2_explorer.py
@@ -11,6 +11,18 @@ from dash_bootstrap_templates import load_figure_template
 dbc_css = 'https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates@V1.0.2/dbc.min.css'
 load_figure_template('bootstrap')
 
+# Import the data
+df = pd.read_csv('world_1960_2021.csv')
+
+# Change year column type
+df['year'] = pd.to_datetime(df['year'], format = '%Y')
+
+# Change column names and to upper
+df.columns = ['COUNTRY', 'YEAR', 'TOTAL_CO2', 'CO2_PER_CAPITA']
+
+# Drop NA
+df.dropna(inplace = True)
+
 app = Dash(__name__, external_stylesheets = [dbc.themes.BOOTSTRAP, dbc_css])
 server = app.server
 
@@ -27,7 +39,44 @@ app.layout = dbc.Container(
         
     ],
     className = 'dbc'
+
+
 )
+
+def plot_co2(df=df):
+
+    # Copy df
+    df_copy = df.copy()
+
+    # Sort data according to year
+    df_copy.sort_values(['YEAR'], inplace = True)
+
+    # Create line plot
+    fig = px.line(
+        df_copy,
+        x = 'YEAR',
+        y = 'TOTAL_CO2'
+    )
+
+    fig.update_layout(
+        yaxis=dict(title='Total CO2'),
+        yaxis2=dict(title='CO2 Per Capita', overlaying='y', side='right',
+                    tickfont=dict(color='red')),
+        xaxis_title = 'CO2 Around the world',
+        showlegend = False
+    )
+
+    fig.add_scatter(x=df['YEAR'],
+                    y=df['CO2_PER_CAPITA'],
+                    mode='lines',
+                    yaxis = 'y2',
+                    hovertext=df['CO2_PER_CAPITA'])
+
+    fig.update_traces(textposition='top center')
+
+    return fig
+
+plot_co2(df)
 
 if __name__ == '__main__':
     app.run_server(debug = True)
